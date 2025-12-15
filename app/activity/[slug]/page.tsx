aa95411df804f5d2501cb9de1abf6783e865f274
juyalrohit <rohitjuyalp205@gmail.com>
2025-12-15 23:33:29 +0530

    11:33 Am 15-5-2025 15th commit

--- app/activity/[slug]/page.tsx
+++ app/activity/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { notFound } from "next/navigation";
 import ClientDetails from "./ClientDetails";
 
-export const revalidate = 60 * 60 * 12; // 12 hours ISR
 
 /* Fetch Single Activity */
 
@@ -9,7 +8,7 @@ async function getActivity(slug: string) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/activity/get/${slug}`,
     {
-      next: { revalidate },
+      next: { revalidate:60 },
     }
   );
 
@@ -20,22 +19,22 @@ async function getActivity(slug: string) {
 }
 
 /* Generate Static Params */
-export async function generateStaticParams() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/activity/get`,
-    {
-      cache: "force-cache",
-    }
-  );
+// export async function generateStaticParams() {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/activity/get`,
+//     {
+//       cache: "force-cache",
+//     }
+//   );
 
-  if (!res.ok) return [];
+//   if (!res.ok) return [];
 
-  const json = await res.json();
+//   const json = await res.json();
 
-  return json.data.map((item: { slug: string }) => ({
-    slug: item.slug,
-  }));
-}
+//   return json.data.map((item: { slug: string }) => ({
+//     slug: item.slug,
+//   }));
+// }
 
 export default async function ActivityPage({params} : {params:Promise<{slug:string}>}){
      const slug = (await params).slug;
